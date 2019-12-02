2d3ba62a034b2a0987185f3144158a7748d9d1d5
Paul93-20 <58366366+Paul93-20@users.noreply.github.com>
2019-12-02 23:39:29 +0500

    Реализация Крестики-нолики ООП

---
+++ src/Game/Game.java
@@ -0,0 +1,32 @@
+package Game;
+
+import javax.swing.*;
+
+public class Game {
+
+    private GameBoard board; //ссылка на игровое поле
+    private GamePlayer[] gamePlayers = new GamePlayer[2]; // массив игроков
+    private int playersTurn = 0; //индекс текущего игрока
+
+    public Game(){
+        this.board = new GameBoard(this);
+    }
+    public void initGame(){
+        gamePlayers[0] = new GamePlayer(true, 'X');
+        gamePlayers[1] = new GamePlayer(false, 'O');
+    }
+
+    //Метод передачи хода
+    void passTurn(){
+        if(playersTurn == 0)
+            playersTurn = 1;
+        else
+            playersTurn = 0;
+    }
+
+    GamePlayer getCurrentPlayer() {return gamePlayers[playersTurn];}
+
+    void showMessage(String messageText){
+        JOptionPane.showMessageDialog(board, messageText);
+    }
+}
---
+++ src/MainClass.java
@@ -0,0 +1,9 @@
+import Game.Game;
+
+
+public class MainClass {
+    public static void main(String[] args) {
+        Game gameInstance = new Game();
+        gameInstance.initGame();
+    }
+}
